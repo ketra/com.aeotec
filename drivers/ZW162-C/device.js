84fd3036f8228d09b2c417d4842310d6790d622d
kevin traa <ketra90@gmail.com>
2020-06-18 12:17:07 +0200

    #ZW162-C Added: - New flowcard for volume - New flowcard for sound. - Added more settings.
    Fixed:
    - incorrect sound names
    - added all the sounds.

--- drivers/ZW162-C/device.js
+++ drivers/ZW162-C/device.js
@@ -1,123 +1,92 @@
-'use strict';
+'use strict'
 
-const { ZwaveDevice } = require('homey-meshdriver');
+const { ZwaveDevice } = require('homey-meshdriver')
 
-const SIREN_TIMEOUT = 10 * 1000;
-const TAMPER_TIMEOUT = 30 * 1000;
+const SIREN_TIMEOUT = 10 * 1000
+const TAMPER_TIMEOUT = 30 * 1000
 
 class AeotecDoorbellSixDevice extends ZwaveDevice {
 
-  onMeshInit() {
-    this.printNode();
-    this.enableDebug();
+  onMeshInit () {
+    this.printNode()
+    this.enableDebug()
 
-    this.currentSiren = 1;
-    this.defaultSiren = Number(this.getSetting('default_sound'));
-	this.ringer_sound = Number(this.getSetting('doorbell_sound'));
+    this.currentSiren = 1
+    this.defaultSiren = Number(this.getSetting('default_sound'))
+    this.ringer_sound = Number(this.getSetting('doorbell_sound'))
 
     this.registerReportListener('NOTIFICATION', 'NOTIFICATION_REPORT', report => {
       if (report['Notification Type'] && report['Notification Status']) {
         if (report['Notification Type'] === 'Siren') {
-          this.setCapabilityValue('onoff.siren', !!report['Notification Status (Raw)']);
+          this.setCapabilityValue('onoff.siren', !!report['Notification Status (Raw)'])
 
-          if (this.sirenTimeout) clearTimeout(this.sirenTimeout);
+          if (this.sirenTimeout) clearTimeout(this.sirenTimeout)
           this.sirenTimeout = setTimeout(() => {
-            this.setCapabilityValue('onoff.siren', false);
-          }, SIREN_TIMEOUT);
+            this.setCapabilityValue('onoff.siren', false)
+          }, SIREN_TIMEOUT)
         }
 
         if (report['Notification Type'] === 'Home Security') {
-          this.setCapabilityValue('alarm_tamper', !!report['Notification Status (Raw)']);
+          this.setCapabilityValue('alarm_tamper', !!report['Notification Status (Raw)'])
 
-          if (this.tamperTimeout) clearTimeout(this.tamperTimeout);
+          if (this.tamperTimeout) clearTimeout(this.tamperTimeout)
           this.tamperTimeout = setTimeout(() => {
-            this.setCapabilityValue('alarm_tamper', false);
-          }, TAMPER_TIMEOUT);
+            this.setCapabilityValue('alarm_tamper', false)
+          }, TAMPER_TIMEOUT)
         }
       }
-    });
+    })
   }
 
-  async setSiren({ sirenNumber = 1, sirenState }) {
-    this.currentSiren = sirenNumber;
-    this.log('Turning siren', sirenNumber, sirenState);
-    return this.node.MultiChannelNodes[`${sirenNumber}`].CommandClass.COMMAND_CLASS_BASIC.BASIC_SET({
-      Value: sirenState,
-    });
+  async setSiren (sound, Volume) {
+    this.log('Turning siren', sound)
+    return this.node.MultiChannelNodes[1].CommandClass['COMMAND_CLASS_SOUND_SWITCH']['SOUND_SWITCH_TONE_PLAY_SET']({ 'Tone identifier': Number(sound) })
   }
 
-  async resetSiren() {
-    this.setCapabilityValue('onoff.siren', false);
+  async setSound (node, sound) {
+    var result = await this.node.MultiChannelNodes[`${node}`].CommandClass['COMMAND_CLASS_SOUND_SWITCH']['SOUND_SWITCH_CONFIGURATION_GET']()
+    var Volume = Number(result['Volume'])
+
+    return this.node.MultiChannelNodes[`${node}`].CommandClass['COMMAND_CLASS_SOUND_SWITCH']['SOUND_SWITCH_CONFIGURATION_SET']({
+      Volume: Volume,
+      'Default Tone Identifier': sound
+    })
+  }
+
+  async resetSiren () {
+    this.setCapabilityValue('onoff.siren', false)
     return this.node.MultiChannelNodes[`${this.currentSiren}`].CommandClass.COMMAND_CLASS_BASIC.BASIC_SET({
-      Value: false,
-    });
+      Value: false
+    })
   }
-  
- async setVolume(node, Volume, sound) {
-	     return this.node.MultiChannelNodes[`${node}`].CommandClass['COMMAND_CLASS_SOUND_SWITCH']['SOUND_SWITCH_CONFIGURATION_SET']({
-		Volume: Volume,
-		'Default Tone Identifier': sound
-		});
- }
 
-  async onSettings(oldSettings, newSettings, changedKeys) {
-    this.log(changedKeys);
-	this.log(oldSettings);
-	
-	// Default Sound Settings.
-    if (changedKeys.includes('default_sound')) {
-      if (Number(newSettings['default_sound']) < 9 && Number(newSettings['default_sound']) > 0) {
-        this.log('Changing default sound');
-        this.defaultSiren = newSettings['default_sound'];
-		if(changedKeys.includes('Default_volume'))
-			this.setVolume(1, newSettings['Default_volume'], Number(newSettings['doorbell_sound']));
-		else
-			this.setVolume(1, oldSettings['Default_volume'], Number(newSettings['doorbell_sound']));
+  async setVolume (node, Volume, sound) {
+    var result = await this.node.MultiChannelNodes[`${node}`].CommandClass['COMMAND_CLASS_SOUND_SWITCH']['SOUND_SWITCH_CONFIGURATION_GET']()
+    if (sound == 0)
+      sound = Number(result['Default Tone Identifer'])
+    return this.node.MultiChannelNodes[`${node}`].CommandClass['COMMAND_CLASS_SOUND_SWITCH']['SOUND_SWITCH_CONFIGURATION_SET']({
+      Volume: Volume,
+      'Default Tone Identifier': sound
+    })
+  }
+
+  async onSettings (oldSettings, newSettings, changedKeys) {
+    changedKeys.forEach(key => {
+      var [type, node] = key.split('_')
+      if (type == 'sound') {
+        if (changedKeys.includes(`volume_${node}`))
+          this.setVolume(node, newSettings[`volume_${node}`], Number(newSettings[`sound_${node}`]))
+        else
+          this.setVolume(node, oldSettings[`volume_${node}`], Number(newSettings[`sound_${node}`]))
       }
-    }
-	if (changedKeys.includes('Default_volume')) {
-		this.log('Changing Volume')
-		this.setVolume(1, newSettings['Default_volume'], Number(this.getSetting('doorbell_sound')))
-	}
-	
-	// Tamper Settings
-	if (changedKeys.includes('tamper_sound')) {
-      if (Number(newSettings['tamper_sound']) < 18 && Number(newSettings['tamper_sound']) > 0) {
-        this.log('Changing default sound');
-        this.defaultSiren = newSettings['tamper_sound'];
-		if(changedKeys.includes('tamper_volume'))
-			this.setVolume(2, newSettings['tamper_volume'], Number(newSettings['tamper_sound']));
-		else
-			this.setVolume(2, oldSettings['tamper_volume'], Number(newSettings['tamper_sound']));
+      if (type == 'volume') {
+        this.setVolume(node, newSettings[`volume_${node}`], Number(this.getSetting(`sound_${node}`)))
       }
-    }
-	if (changedKeys.includes('tamper_volume')) {
-		this.log('Changing Volume')
-		this.setVolume(2, newSettings['tamper_volume'], Number(this.getSetting('doorbell_sound')))
-	}
-	
-	// Button 1 Settings
-	if (changedKeys.includes('doorbell_sound')) {
-      if (Number(newSettings['doorbell_sound']) < 9 && Number(newSettings['doorbell_sound']) > 0) {
-        this.log('Changing ringer sound');
-        this.ringer_sound = Number(newSettings['doorbell_sound']);
-		if(changedKeys.includes('ringer_volume'))
-			this.setVolume(3, newSettings['ringer_volume'], Number(newSettings['doorbell_sound']));
-		else
-			this.setVolume(3, oldSettings['ringer_volume'], Number(newSettings['doorbell_sound']));
-      }
-    }
-	if (changedKeys.includes('ringer_volume')) {
-		this.log('Changing Volume')
-		this.setVolume(3, newSettings['ringer_volume'], Number(this.getSetting('doorbell_sound')));
-	}
+    })
 
-
-	
-
-    return Promise.resolve(newSettings);
+    return Promise.resolve(newSettings)
   }
 
 }
 
-module.exports = AeotecDoorbellSixDevice;
+module.exports = AeotecDoorbellSixDevice
--- drivers/ZW162-C/driver.js
+++ drivers/ZW162-C/driver.js
@@ -1,25 +1,42 @@
-'use strict';
+'use strict'
 
-const Homey = require('homey');
+const Homey = require('homey')
 
 class AeotecDoorbellSixDriver extends Homey.Driver {
 
-  onInit() {
-    super.onInit();
+  onInit () {
+    super.onInit()
 
     this.alarmOnFlow = new Homey.FlowCardAction('ZW162-turn_alarm_on')
       .register()
       .registerRunListener(async (args, state) => {
-        if (typeof (args.sound) !== 'number') return new Error('Sound should be a number');
-        return args.device.setSiren({ sirenNumber: args.sound, sirenState: true });
-      });
+        // console.log(args)
+        // console.log(state)
+        // if (typeof (args.sound) !== 'number') return new Error('Sound should be a number')
+        var result = false
+        result = args.device.setSiren(Number(args.sound), Number(args.Vomule)).then(value => {
+          console.log(value); // Success!
+        }, reason => {
+          console.error(reason); // Error!
+        })
+      })
     this.alarmOffFlow = new Homey.FlowCardAction('ZW162-turn_alarm_off')
       .register()
       .registerRunListener(async (args, state) => {
-        return args.device.resetSiren();
-      });
+        return args.device.resetSiren()
+      })
+    this.alarmOffFlow = new Homey.FlowCardAction('ZW162-change_volume')
+      .register()
+      .registerRunListener(async (args, state) => {
+        return args.device.setVolume(Number(args.item), Number(args.volume), 0)
+      })
+    this.alarmOffFlow = new Homey.FlowCardAction('ZW162-change_Sound')
+      .register()
+      .registerRunListener(async (args, state) => {
+        return args.device.setSound(Number(args.item), Number(args.sound))
+      })
   }
 
 }
 
-module.exports = AeotecDoorbellSixDriver;
+module.exports = AeotecDoorbellSixDriver
